e5b283882e92a223a3c67bbd4b41efaee46fd72a
Chris Reidy <chris.reidy@timeinc.com>
2018-04-18 23:01:19 +0100

    Add a slight time delay before items appear on scroll

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ const intersectionObserver = new IntersectionObserver((entries) => {
 	entries.forEach(entry => {
 		let entryClasses = entry.target.classList;
 		if(entry.intersectionRatio > 0) {
-			console.log(entryClasses);
-			// intersectionObserver.unobserve(entry.target);
+			setTimeout(function(){
+				entryClasses.add('is-in-view');
+				intersectionObserver.unobserve(entry.target);
+			}, 300);
 		}
 	});
 });
